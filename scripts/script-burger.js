3e39107f7654b8c5b26cf9883d715671648ca920
Mathias0112546 <113148252+123-Mathias@users.noreply.github.com>
2024-04-23 15:35:02 -0500

    fix: Script section to alert
    In this part, we improved the alert in javascript

--- scripts/script-burger.js
+++ scripts/script-burger.js
@@ -11,3 +11,12 @@ function mostrarMensaje() {
     alert("¡Registro exitoso! Gracias por unirse al cambio :)");
     return true;    
 }
+
+function mostrarMensaje() {
+    Swal.fire({
+        title: "Registro exitoso",
+        text: "Te contactaremos pronto",
+        icon: "success"
+      });
+      return false;
+}
